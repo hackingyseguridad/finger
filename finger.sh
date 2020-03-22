c2a39beff946419bca8ac39bd58fa5ca01c3c855
hackingyseguridad <antonio_taboada@hotmail.es>
2020-03-22 22:35:06 +0100

    Update finger.sh

--- finger.sh
+++ finger.sh
@@ -1,4 +1,15 @@
 #!/bin/bash 
+#!/bin/bash 
+cat << "INFO"
 
-
+INFO
+if [ -z "$1" ]; then
+        echo
+        echo "Tcp79/Finger."
+        echo "Uso.: sh finger.sh <ip>"
+        echo
+        exit 0
+fi
+echo
+echo
 for i in {0..9};do finger -l $i@$1;done|sort -u
